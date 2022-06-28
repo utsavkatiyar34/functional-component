8305848e75c1186b5646de5fc50b67563a3bf0c9
utsavkatiyar34 <utsavkatiyar34@gmail.com>
2022-06-28 15:15:33 +0530

    done

---
+++ card/src/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+        <h2>Mobile Operating System</h2>
+        <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+        </ul>
+        <h2>Mobile Manufacturers</h2>
+        <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
